c79ded2daf6a4764994bd897ce26d36561fe6f1b
ImAgremlin1123 <liamgleason5@gmail.com>
2024-11-18 09:31:42 -0500

    Add files via upload

--- CustomerOrders.py
+++ CustomerOrders.py
@@ -9,11 +9,18 @@
 # IMPORT PYTHON MODULES =======================================================
 
 from random import * # [NP] This calls the random module, which let's the function obtain random numbers
-
+import Molecules as mol #[LG] Imports all our different molecules and recipies to be used in functions
 # ORDER LIST ===============================================================
 
 # [NP] The chemical list shows all possible chemicals that a customer can order
 Chemicals = ["NaCl (Sodium Chloride)", "NH3 (Ammonia)", "NH4OH (Ammonium Hydroxide)", "HCl (Hydrochloric Acid)", "NaOH (Sodium Hydroxide)"]
+NaCl = mol.NaCl()
+NH3 = mol.NH3()
+NH4OH = mol.NH4OH()
+HCl = mol.HCl()
+NaOH = mol.NaOH()
+Chemicals = [NaCl,NH3,NH4OH,HCl,NaOH] # [LG] generates a generic object of every recipie to ask the player for, and latter compare to what is produced
+
 
 # CUSTOMER ORDER ==============================================================
    
@@ -24,14 +31,17 @@ def CustomerOrder(): # [NP] Generates a Customer Order
 
     Returns
     -------
-    list
-        [Quantity, Chemical]
+    Object with assigned quanity
+        
 
     '''
     
     orderchem = Chemicals[randint(0, len(Chemicals) - 1)] # [NP] Picks a random chemical from the chemical list
     ordercapacity = randint(1, 999) # [NP] Picks a random quantity of the chemical
-    
-    return [str(ordercapacity), str(orderchem)] # [NP] Return the customer order details back
+    orderchem.Quantity =+ ordercapacity # [LG] Assigns the Amount of desired product to the object
+    return orderchem # [NP] Return the customer order details back
 
 # END =========================================================================
+mix = CustomerOrder()
+print(type(mix))
+print (mix.Quantity)
