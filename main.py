9695748d211e7da744639fda04cb8c7a1b3e9916
katieeyu <109837166+katieeyu@users.noreply.github.com>
2024-11-19 10:55:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,8 @@
 # any way, but it calls all other files (or modules) in sequence which allows
 # the game to flow and function.
 
-# Author: Nicholas Pribytkov ==================================================
+# Author: Nicholas Pribytkov
+# Editors: Katie Yu ==================================================
 
 # IMPORT PY FILES =============================================================
 
@@ -41,26 +42,24 @@ print(b)
 WorkingMovingGoose.Runtime(player, position, OrderA, OrderB, False, Order1, Order2)
 
 ---------------------------------------------------------------------------------------------------------
-#[KY] run order_match here with objects returned by customer orders and chemical produced functions
-order_match(orderchem, producedchem)
 
-#initialize variables required for order_match to work
-#once player presses "done" on the machine, end stopwatch that begins when player accepts order and store time_taken as a variable
+[KY] #initialize variables required for order_match to work
+[KY] #once player presses "done" on the machine, end stopwatch that begins when player accepts order and store time_taken as a variable
 
 wait_time = #set equal to timer value
 time_taken = #set equal to stopwatch value
 time_difference = wait_time - time_taken
 accuracies_below_30 = 0
 
-#Refer to order match function to see how the below variables are defined
+---------------------------------------------------------------------------------------------------------
+
+# [KY] Check to see if game has ended (put in its own file?)
+# [KY] Refer to order match function to see how the below variables are defined
 if accuracy_percent < 30 or chem_match == False:
          accuracies_below_30 += 1
 
-#Check to see if game has ended 
-#repeat all function calls here or time_taken >= wait_time
 if accuracies_below_30 == 3 or time_taken >= wait_time:
-         #end game here
-         break
+         #end game here (break)
 
 else:
          #repeat code for another order
