ec14b776d0b2e0f6d31b7e00aa327d306a44d78a
katieeyu <109837166+katieeyu@users.noreply.github.com>
2024-11-19 13:10:19 -0500

    Update Working_Order_Match_Function.py

--- Working_Order_Match_Function.py
+++ Working_Order_Match_Function.py
@@ -12,8 +12,6 @@
 # Editor(s): Nicholas Pribytkov ===============================================
 
 def order_match(chem_in, chem_desired):
-# [KY] Default arguments allow us to compare the variables from the previous two functions
-# [KY] Variables from previous two functions are chem_produced and chem_ordered
     
     if type(chem_in) == type(chem_desired):
         # [KY] This compares the names of the chemical produced and the chemical ordered
@@ -21,36 +19,31 @@ def order_match(chem_in, chem_desired):
         chem_match = True
         # [KY] Take abs value of error
         accuracy = abs((chem_in.quantity - chem_desired.quantity)/chem_desired.quantity)
-        # [KY] If customer orders 2 mol and you make 1.75, accuracy = 0.875
+        # [KY] Sample accuracy: if customer orders 2 mol and you make 1.75, accuracy = 0.875
 
         accuracy_percent = accuracy*100
             
         points_per_order = int(chem_desired.difficulty + time_difference)*accuracy
         # [KY] Sample points_per_order = (20 + (20-10))*0.8 = 24
         
-        return points_per_order
-        return accuracy_percent
-        # [KY] Accuracy should be very low for a fail to occur
-        # Use accuracy to determine fail
-        
     else:
+        # [KY] If the player makes the completely wrong chemical
         chem_match = False
         points_per_order = 0
         accuracy_percent = 0
-        accuracies_below_30 += 1
-        
-        # [KY] If the player makes the completely wrong chemical
+
+        return points_per_order
+        return accuracy_percent
+
+        # [KY] Accuracy should be very low for a fail to occur
+        # Use accuracy to determine fail
             
         if chem_match == True:
             # [KY] Make goose leave here (happy)
+            total_points += points_per_order
+            return total_points
 
         if chem_match == False:
             # [KY] Make goose leave here (sad)
-
-# [KY] Put in main file
-            # time_points = 2 
-            # [KY] initialize time_points (if this is what we want)
-            # [KY] Start with a low value for time_points since the player will have the most time to make the recipe
-            
             total_points += points_per_order
             return total_points
