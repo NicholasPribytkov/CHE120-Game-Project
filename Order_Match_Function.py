76ef2ece8a1f4eaf9af7186989f095a310244d03
katieeyu <109837166+katieeyu@users.noreply.github.com>
2024-11-21 01:02:21 -0500

    Update Order_Match_Function.py

--- Order_Match_Function.py
+++ Order_Match_Function.py
@@ -19,24 +19,26 @@ def order_match(chem_in, chem_desired):
         
         #chem_match = True variable assignment isn't necessary since this case is unlikely, unless we want to use it for something
         # [KY] Take abs value of error
-        accuracy = abs((chem_in.quantity - chem_desired.quantity)/chem_desired.quantity)
+        accuracy_fraction = abs((chem_in.quantity - chem_desired.quantity)/chem_desired.quantity)
         # [KY] Sample accuracy: if customer orders 2 mol and you make 1.75, accuracy = 0.875
-
-        accuracy_percent = accuracy*100
             
-        points_per_order = int(chem_desired.difficulty + time_difference)*accuracy
+        points_per_order = int((chem_desired.difficulty + time_difference)*(accuracy_fraction))
         # [KY] Sample points_per_order = (20 + (20-10))*0.8 = 24
         
     else:
         # [KY] If the player makes the completely wrong chemical
         #chem_match = False same comment as above
         points_per_order = 0
-        accuracy_percent = 0
 
     return points_per_order
 
-    def accuracy_as_percent(chem_in_amount, chem_desired_amount):
-        accuracy_percent = abs((chem_in_amount - chem_desired_amount)/chem_desired_amount)*100
+    def accuracy_as_percent(chem_in, chem_desired):
+        if type(chem_in) == type(chem_desired):
+            accuracy_percent = (abs((chem_in.quantity - chem_desired.quantity)/chem_desired.quantity))*100
+
+        else:
+            # [KY] If the player makes the completely wrong chemical, accuracy is automatically 0
+            accuracy_percent = 0
         
         return accuracy_percent
         
