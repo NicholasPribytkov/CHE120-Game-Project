b502e105813023657a76f44b3e238d7e24b1bca1
katieeyu <109837166+katieeyu@users.noreply.github.com>
2024-11-21 22:33:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,7 @@
 
 # IMPORT PY FILES =============================================================
 
-#import rest of files
+#[KY] import rest of files
 import CustomerOrders
 import Mixing_Function
 import Molecules
@@ -44,7 +44,7 @@ import TheMachine
 import Order_Match_Function
 import WorkingMovingGoose # [NP] WorkingMovingGoose outputs visuals of the Customers and Orders
 
-#import pygame for items to display at the end
+#[KY] import pygame for end of game messages
 import pygame
 import pygame.freetype
 #import TheMachine # [NP] TheMachine outputs visuals of the Machine at work, allowing the player to interface with it in order to mix chemicals together.
@@ -61,16 +61,16 @@ WorkingMovingGoose.Runtime(player, position, OrderA, OrderB, False, Order1, Orde
 
 # Order_Match must be able to access the below variables somehow as they are used in the point system
 
-wait_time = #placeholder, store customer wait time
-time_taken = #placeholder, store time elapsed from accepting an order to finishing the order here
+wait_time = Order_Match_Function.waittime #placeholder, customer wait time (doesn't exist yet)
+time_taken = Order_Match_Function.timetaken #placeholder, time elapsed from accepting an order to finishing the order here (doesn't exist yet)
 
 # [KY] the below variables are adjusted by order_match until the game ends
 # they must be initialized in main so that their values both start at 0
-# initializing them in main  also allows them to be easily accessed at the end of the game
+# initializing them in main also allows them to be easily accessed at the end of the game
 # initializing them before the loop allows their values to accumulate as the order_match function is repeated
+
 accuracies_below_30 = 0
 total_points = 0
-
 
 # ---------------------------------------------------------------------------------------------------------
 #[KY] Initialize loop that runs an order and call functions
@@ -134,11 +134,14 @@ def playAgain():
                   PlayerFail = True
 
 if PlayerFail:
-         playAgain():
-         #allow player to play again?
-         #reset values to 0 
-         accuracies_below_30 = 0
-         total_points = 0
+         if playAgain():
+                  #allow player to play again
+                  #reset values to 0 
+                  accuracies_below_30 = 0
+                  total_points = 0
+         else:
+                  break
+         
          
 
                   
