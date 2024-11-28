83182f48dcdffe806dd1db865097692083b5a4f9
katieeyu <109837166+katieeyu@users.noreply.github.com>
2024-11-27 21:24:23 -0500

    Update Point_Function.py

--- Point_Function.py
+++ Point_Function.py
@@ -7,9 +7,11 @@
 # Author: Katie Yu ============================================================
 # Editor(s): Nicholas Pribytkov ===============================================
 
-def point_calculation(Difficulty, accuracy_fraction, time_difference):
+def point_calculation(Difficulty, accuracy_fraction, time_fraction):
     
-    points_per_order = int((Difficulty + time_difference)*(accuracy_fraction))
-    # [KY] Sample points_per_order = (20 + (20-10))*0.8 = 24
+    points_per_order = int((Difficulty + time_fraction)*accuracy_fraction))
+    # [KY] Sample points_per_order = ((20 + ((20-10)/20))*0.8) = 16
+
+    # [KY] time_fraction is (time given - time taken)/(time given)
 
     return points_per_order
