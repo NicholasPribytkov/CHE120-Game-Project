b1fe63305d4b934cd586582ac9d347075a2a2374
N01Liam <lawestla@uwaterloo.ca>
2024-11-29 11:02:52 -0500

    Update GooseUpdates.py
    Mole display put in correct position

--- GooseUpdates.py
+++ GooseUpdates.py
@@ -628,17 +628,17 @@ def Game(Score): # [NP] The score parameter determines how much score the player
             if mix1 != None:
                 element1 = mix1.Name
                 Moly1= str(element1)+ ' = ' + str(mix1.Quantity)
-                display_text2(Moly1,25,150)# Currently place holder will need to be repositioned
+                display_text2(Moly1,700,650)# Currently place holder will need to be repositioned
                 pygame.display.update()
             if mix2 != None:
                 element2 = mix2.Name
                 Moly2= str(element2)+ ' = ' + str(mix2.Quantity)
-                display_text2(Moly2,225,150)
+                display_text2(Moly2,700,700)
                 pygame.display.update()
             if mix3 != None:
                 element3 = mix3.Name
                 Moly3= str(element3)+ ' = ' + str(mix3.Quantity)
-                display_text2(Moly3,425,150)
+                display_text2(Moly3,700,750)
                 pygame.display.update()
 
         if Show_FlaskCOPY: screen.blit(FlaskCOPY, FlaskCopyPos)  # [LAW] Display FlaskCOPY at the new position
