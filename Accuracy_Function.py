c029cf4f8095a3fbd26af3f7735ddb2d915a7354
katieeyu <109837166+katieeyu@users.noreply.github.com>
2024-11-30 04:20:16 -0500

    Update Accuracy_Function.py

--- Accuracy_Function.py
+++ Accuracy_Function.py
@@ -10,21 +10,26 @@
 
 # FUNCTION ====================================================================
 
-def accuracy_as_percent(chem_in, chem_desired, chem_in_quantity, chem_desired_quantity):
+def accuracy_as_percent(chem_made, chem_desired, chem_made_quantity, chem_desired_quantity):
     
-        # [NP] chem_in represents the chemical that the player has made and chem_desired is the chemical that is outlined in the order
-        # [NP] a and b represent the quantities of the player chemical and the order chemical respectively (shortened to make it easier to see the formula)
+        # [NP] chem_made represents the chemical produced and chem_desired is the chemical ordered
 
-        if type(chem_in) == type(chem_desired): # [NP] Check if the player chemical and the order chemical are the same
-            accuracy_percent = (1 - abs(chem_in_quantity - chem_desired_quantity)/chem_desired_quantity) * 100
+        if type(chem_made) == type(chem_desired): # [NP] Check if the player chemical and the order chemical are the same
+            if chem_made_quantity <= 2*chem_desired_quantity:
+                accuracy_percent = (1 - abs(chem_made_quantity - chem_desired_quantity)/chem_desired_quantity) * 100
+                
+                # [KY] absolute value accounts for the user making either too much or too little of a chemical (so 7 mols produced and 5 mols ordered would return same accuracy as 3 mols produced and 5 mols ordered)
+                # [KY] Sample accuracy: if customer orders 2 mol and you make 1.75, accuracy = 0.875
             
-        # [KY] absolute value accounts for the user making either too much or too little of a chemical
-        # [KY] Sample accuracy: if customer orders 2 mol and you make 1.75, accuracy = 0.875
-        
+            elif chem_made_quantity > 2*chem_desired_quantity:
+                    accuracy_percent = 0
+                    
+                # [KY] prevents function from returning a negative accuracy value if you make more than double the amount ordered
+                    
         else:
+            accuracy_percent = 0
             
             # [KY] If the player makes the completely wrong chemical, accuracy is automatically 0
-            accuracy_percent = 0
             
         return accuracy_percent
 
